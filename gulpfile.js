e27ad2c2c6133a0b302bd0ec308688aa47563928
Acris Liu <acrisliu@gmail.com>
2019-01-13 20:48:14 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,37 @@
-var gulp = require('gulp');
-var pump = require('pump');
-var cleanCSS = require('gulp-clean-css');
-var uglify = require('gulp-uglify');
-var htmlclean = require('gulp-htmlclean');
-var htmlmin = require('gulp-htmlmin');
-var babel = require('gulp-babel');
+var gulp = require('gulp')
+var cleanCSS = require('gulp-clean-css')
+var uglify = require('gulp-uglify')
+var htmlclean = require('gulp-htmlclean')
+var htmlmin = require('gulp-htmlmin')
+var babel = require('gulp-babel')
 
-// 压缩 public 目录 css
-gulp.task('minify-css', function (cb) {
-    pump([
-            gulp.src('./public/**/*.css'),
-            cleanCSS({compatibility: 'ie9'}),
-            gulp.dest('./public')
-        ],
-        cb
-    );
-});
-// 压缩 public 目录 html
-gulp.task('minify-html', function (cb) {
-    pump([
-            gulp.src('./public/**/*.html'),
-            htmlmin({
-                removeComments: true,
-                minifyJS: true,
-                minifyCSS: true,
-                minifyURLs: true,
-                collapseWhitespace: true
-            }),
-            htmlclean(),
-            gulp.dest('./public')
-        ],
-        cb
-    );
-});
-// 压缩 public/js 目录 js
-gulp.task('minify-js', function (cb) {
-    pump([
-            gulp.src('./public/**/*.js'),
-            babel({presets: ['env']}),
-            uglify(),
-            gulp.dest('./public')
-        ],
-        cb
-    );
-});
-// 执行 gulp 命令时执行的任务
-gulp.task('default', [
-    'minify-html', 'minify-css', 'minify-js'
-]);
+function cssMinify() {
+    return gulp.src('./public/**/*.css')
+        .pipe(cleanCSS({
+            compatibility: 'ie9'
+        }))
+        .pipe(gulp.dest('./public'))
+}
+
+function htmlMinify(cb) {
+    return gulp.src('./public/**/*.html')
+        .pipe(htmlmin({
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('./public'))
+}
+
+function jsMinify(cb) {
+    return gulp.src('./public/**/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }),)
+        .pipe(uglify())
+        .pipe(gulp.dest('./public'))
+}
+
+exports.default = gulp.series(clean, gulp.parallel(cssMinify, htmlMinify, jsMinify))
